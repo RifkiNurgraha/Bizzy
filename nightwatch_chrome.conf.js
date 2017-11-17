183f7cef7af5d522bad0533caa87891beded996e
vagrant <vagrant@vagrant.vm>
2017-11-17 00:07:40 +0000

    change devdependencies

--- nightwatch_chrome.conf.js
+++ nightwatch_chrome.conf.js
@@ -24,7 +24,8 @@ module.exports = {
   disable_colors: false,                            //scenarios colors
   selenium: {                                       //selenium settings
     start_process: true,
-    server_path: './lib/selenium-server-standalone-3.7.1.jar',
+    // server_path: './lib/selenium-server-standalone-3.7.1.jar',
+    server_path: seleniumServer.path,
     log_path: 'logs',
     host: '127.0.0.1',
     port: 4444
@@ -54,7 +55,8 @@ module.exports = {
       },
       selenium: {
         cli_args: {
-          'webdriver.chrome.driver': './lib/chromedriver'
+          // 'webdriver.chrome.driver': './lib/chromedriver'
+          'webdriver.chrome.driver': chromedriver.path
         }
       }
     }
