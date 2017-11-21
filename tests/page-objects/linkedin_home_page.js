4ae02d28c5d59f2da6ee3b260ff0d5ce969c5e86
Genta Wibowo <tetrahedral@gmail.com>
2017-11-21 15:33:45 +0700

    change linkedin .env url

--- tests/page-objects/linkedin_home_page.js
+++ tests/page-objects/linkedin_home_page.js
@@ -3,7 +3,7 @@ var base = require('./../helper/base_page.js');
 
 var linkedinHome = {
     url: function set_url() {
-      var url = process.env.BASE_URL_FACEBOOK;
+      var url = process.env.BASE_URL_LINKEDIN;
       console.log(url);
       return url;
     },
