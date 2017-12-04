bda0ba1982ff838d28da5eb30e357e04cc8b7ae0
fergadipa <ferga.dipa@bizzy.co.id>
2017-12-04 10:25:43 +0700

    Create new PageObject for Business Document page

---
+++ tests/page-objects/growth/business_document.js
@@ -0,0 +1,35 @@
+import { BADSTR } from 'dns';
+
+const env = require('dotenv').config();
+var base = require('./../../helper/base_page.js');
+
+var businessDoc = {
+    elements:{
+        txtNPWP:'',
+        fileNPWP:'',
+        fileSIUP:'',
+        txtExpiredSIUP:'',
+        btnBack:'',
+        btnSaveAndCont:''
+    },
+    commands:[{
+        inputNPWPNumber:function(npwpNumber){
+            return base.setValueElement(this,'@txtNPWP',npwpNumber);
+        },
+        attachNPWPFile:function(){
+            var fileNPWPlocation = "";
+            return base.setValueElement(this,'@fileNPWP',fileNPWPlocation);
+        },
+        attachSIUPFile:function(){
+            var fileSIUPlocation= "";
+            return base.setValueElement(this,'@fileSIUP',fileSIUPlocation);
+        },
+        inputExpiredSIUP:function(expiredSIUP){
+            return base.setValueElement(this,'@txtExpiredSIUP',expiredSIUP);
+        },
+        clickSaveAndCont:function(){
+            return base.clickElement(this,'@btnSaveAndCont');
+        }
+    }]
+}
+module.exports=businessDoc;
