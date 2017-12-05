4e7a7eb863f391b68abc31af0325d9519c208245
fergadipa <ferga.dipa@bizzy.co.id>
2017-12-05 15:50:18 +0700

    Create API test for register using existing account

---
+++ tests/helper/test_data_growth.js
@@ -0,0 +1,12 @@
+var existingAccountData = ({
+    "user_login": "ferga.dipa@bizzy.co.id",
+    "first_name": "Ferga",
+    "middle_name": "",
+    "last_name": "Dipa",
+    "password": "12345678",
+    "role": "user"
+})
+
+module.exports = {
+    existingAccountData: existingAccountData
+}
