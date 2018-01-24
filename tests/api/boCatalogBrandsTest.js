bb9571585fb77e91695a2d028258fa7c4669331f
yanitaveronica <yanita.veronica@bizzy.co.id>
2018-01-24 07:07:04 +0000

    testyanita3

---
+++ tests/api/boCatalogBrandsTest.js
@@ -0,0 +1,5 @@
+const should = require('chai').should();
+const expect = require('chai').expect;
+const common = require('./../helper/common.js');
+const testData = require('./../helper/testData.js');
+var cartSession = require('./../page-objects/api/boCatalogBrands.js');
